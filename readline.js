201f2278e612c346840f61ac819401b458f7720b
Muhammad Asharib <164147267+MahnoorAsim15@users.noreply.github.com>
2024-03-25 12:50:25 +0500

    Initial commit

---
+++ readline.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+}
+
+);
+
+r1.question("what is your name?",function (answer){
+    console.log(`oh,so your name is ${answer}`);
+    r1.close();
+});
---
+++ readprompt.js
@@ -0,0 +1,4 @@
+const prompt = require("prompt-sync")();
+
+const input = prompt("what is your name? ");
+console.log(`oh,so your name is ${input}`);
